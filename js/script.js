ab47b3161796f8b99c02798d391cd5c1fb3e912c
IR~SAGE <sibo.egide98@gmail.com>
2021-07-17 10:21:04 +0200

    Retriving Jobs

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
 document.querySelector(".button-container").addEventListener("click", () => {
-    let text = document.getElementById("filter-jobs").value;
-    console.log(text)
+  let text = document.getElementById("filter-jobs").value;
+  console.log(text);
 });
+
+function getJobs() {
+  return fetch("data.json")
+    .then((Response) => Response.json())
+    .then((data) => {
+      return data;
+    });
+}
+
+getJobs().then((data) => {
+  showJobs(data);
+});
+
+function showJobs(jobs) {}
