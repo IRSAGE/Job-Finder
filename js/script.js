1e55e8797452d41d76d1091214024056ae017ebf
IR~SAGE <sibo.egide98@gmail.com>
2021-07-17 10:47:25 +0200

    Displaying Jobs

--- js/script.js
+++ js/script.js
@@ -15,4 +15,33 @@ getJobs().then((data) => {
   showJobs(data);
 });
 
-function showJobs(jobs) {}
+function showJobs(jobs) {
+  let jobContainer = document.querySelector(".jobs-container");
+  let jobsHtml = "";
+
+  jobs.forEach((job) => {
+    jobsHtml += `
+        <div class="job-tile">
+                <div class="top">
+                    <img src="${job.logo}" alt="tile-image">
+                    <span class="material-icons more_horiz">more_horiz</span>
+                </div>
+                <div class="rolename">
+                    <span>${job.roleName}</span>
+                </div>
+                <div class="description">
+                    <span>${job.requirements.content}</span>
+                </div>
+                <div class="buttons">
+                    <div class="button apply-now">
+                        Apply Now
+                    </div>
+                    <div class="button">
+                        Message
+                    </div>
+                </div>
+            </div>
+        `;
+  });
+  jobContainer.innerHTML = jobsHtml;
+}
